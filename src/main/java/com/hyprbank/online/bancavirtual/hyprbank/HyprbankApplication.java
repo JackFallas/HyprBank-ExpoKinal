1c4c74f4243eb6b807048b42610afcfa32afbbdb
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2025-06-18 00:54:59 -0600

    Proyecto base cargado
    Cargada la base de SpringBoot para empezar a trabajar

---
+++ src/main/java/com/hyprbank/online/bancavirtual/hyprbank/HyprbankApplication.java
@@ -0,0 +1,13 @@
+package com.hyprbank.online.bancavirtual.hyprbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyprbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyprbankApplication.class, args);
+	}
+
+}
