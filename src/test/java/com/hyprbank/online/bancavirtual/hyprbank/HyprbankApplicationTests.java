8d24c3fcffe77a594788a6cd7ca4d242b71a95bf
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2025-06-22 14:42:51 -0600

    Archivos base
    Se suben los archivos base para el proyecto de SpringBot y demas archivos

---
+++ src/test/java/com/hyprbank/online/bancavirtual/hyprbank/HyprbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyprbank.online.bancavirtual.hyprbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyprbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
