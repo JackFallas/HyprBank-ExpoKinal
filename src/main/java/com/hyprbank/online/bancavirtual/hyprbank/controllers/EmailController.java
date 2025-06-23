a54030764eacdd3cc091ce29be91a2f87840a69c
eGalv3zDev <acamposeco-2024018@kinal.edu.gt>
2025-06-23 12:56:06 -0600

    Email Service
    clase para enviar los correos

--- src/main/java/com/hyprbank/online/bancavirtual/hyprbank/controllers/EmailController.java
+++ src/main/java/com/hyprbank/online/bancavirtual/hyprbank/controllers/EmailController.java
@@ -1,5 +1,28 @@
 package com.hyprbank.online.bancavirtual.hyprbank.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hyprbank.online.bancavirtual.hyprbank.services.models.EmailDTO;
+import com.hyprbank.online.bancavirtual.hyprbank.services.models.IEmailService;
+
+import jakarta.mail.MessagingException;
+
+@RestController
+@RequestMapping
 public class EmailController {
     
+    @Autowired
+    IEmailService emailService;
+
+    @PostMapping("/send-email")
+    private ResponseEntity<String> sendEmail(@RequestBody EmailDTO email) throws MessagingException{
+        emailService.sendMail(email);
+        return new ResponseEntity<>("Correo Enviado Exitosamente", HttpStatus.OK);
+    }
 }
